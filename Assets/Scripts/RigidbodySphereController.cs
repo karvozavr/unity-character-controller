11cb5656f37cd7687d3069a88e5160f77473385d
Dmitrii Abramov <karvozavr@gmail.com>
2020-10-24 20:49:52 +0300

    Fix code style

--- Assets/Scripts/RigidbodySphereController.cs
+++ Assets/Scripts/RigidbodySphereController.cs
@@ -4,61 +4,56 @@ using UnityEngine;
 
 public class RigidbodySphereController : MonoBehaviour
 {
-    private Rigidbody body;
+    private Rigidbody _body;
 
-    [SerializeField, Range(0f, 100f)] 
-    float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
 
-    [SerializeField, Range(0f, 100f)] 
-    float maxAcceleration = 10f;
-    
-    [SerializeField, Range(0f, 100f)] 
-    float maxAirAcceleration = 1f;
+    [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
 
-    [SerializeField, Range(0f, 10f)] 
-    float jumpHeight = 2f;
+    [SerializeField, Range(0f, 100f)] float maxAirAcceleration = 1f;
 
-    [SerializeField, Range(0, 5)] 
-    int maxAirJumps = 0;
+    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
 
-    private Vector3 velocity;
+    [SerializeField, Range(0, 5)] int maxAirJumps = 0;
 
-    private Vector3 desiredVelocity;
+    private Vector3 _velocity;
 
-    private bool desiredJump;
+    private Vector3 _desiredVelocity;
 
-    private int jumpPhase;
+    private bool _desiredJump;
 
-    private bool onTheGround;
+    private int _jumpPhase;
 
-    void Awake()
+    private bool _onTheGround;
+
+    private void Awake()
     {
-        body = GetComponent<Rigidbody>();
+        _body = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1.0f);
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        _desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
 
-        desiredJump |= Input.GetButtonDown("Jump");
+        _desiredJump |= Input.GetButtonDown("Jump");
     }
 
     void FixedUpdate()
     {
         UpdateState();
-        if (desiredJump)
+        if (_desiredJump)
         {
-            desiredJump = false;
+            _desiredJump = false;
             Jump();
         }
 
-        AdjustVelocity(desiredVelocity);
+        AdjustVelocity(_desiredVelocity);
 
-        onTheGround = false;
+        _onTheGround = false;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -76,39 +71,40 @@ public class RigidbodySphereController : MonoBehaviour
         foreach (var contactPoint in collision.contacts)
         {
             Vector3 normal = contactPoint.normal;
-            onTheGround |= normal.y >= 0.9f;
+            _onTheGround |= normal.y >= 0.9f;
         }
     }
 
     private void Jump()
     {
-        if (onTheGround || jumpPhase < maxAirJumps)
+        if (_onTheGround || _jumpPhase < maxAirJumps)
         {
-            jumpPhase += 1;
+            _jumpPhase += 1;
             float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-            if (velocity.y > 0)
+            if (_velocity.y > 0)
             {
-                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+                jumpSpeed = Mathf.Max(jumpSpeed - _velocity.y, 0f);
             }
-            velocity.y += jumpSpeed;
+
+            _velocity.y += jumpSpeed;
         }
     }
 
     private void UpdateState()
     {
-        velocity = body.velocity;
-        if (onTheGround)
+        _velocity = _body.velocity;
+        if (_onTheGround)
         {
-            jumpPhase = 0;
+            _jumpPhase = 0;
         }
     }
-    
+
     private void AdjustVelocity(Vector3 desiredVelocity)
     {
-        float acceleration = onTheGround ? maxAcceleration : maxAirAcceleration;
+        float acceleration = _onTheGround ? maxAcceleration : maxAirAcceleration;
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        body.velocity = velocity;
+        _velocity.x = Mathf.MoveTowards(_velocity.x, desiredVelocity.x, maxSpeedChange);
+        _velocity.z = Mathf.MoveTowards(_velocity.z, desiredVelocity.z, maxSpeedChange);
+        _body.velocity = _velocity;
     }
 }
