71142dafa5d9728e022f5e133dc06e9ebd5a887b
Dmitrii Abramov <karvozavr@gmail.com>
2020-10-24 13:01:52 +0300

    Add acceleration to movement

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class MovingSphere : MonoBehaviour {
 
     [SerializeField, Range(0f, 100f)]
-    public float maxSpeed = 10f;
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+    
+    Vector3 velocity;
 
     void Update() {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1.0f);
-        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
     }
