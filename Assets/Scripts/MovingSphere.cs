612dbd1cf0ad55cc668ff0dfbc274080796afeb0
Dmitrii Abramov <karvozavr@gmail.com>
2020-10-24 12:30:36 +0300

    Implement simple input-based movement

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -2,4 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingSphere : MonoBehaviour {}
+public class MovingSphere : MonoBehaviour {
+
+    void Update() {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
+    }
+}
