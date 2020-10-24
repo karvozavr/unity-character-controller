a3f1a6b08f4a41814ddc83cf43bf0118f2ec7dd0
Dmitrii Abramov <karvozavr@gmail.com>
2020-10-24 12:45:22 +0300

    Add velocity and max speed

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour {
 
+    [SerializeField, Range(0f, 100f)]
+    public float maxSpeed = 10f;
+
     void Update() {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        playerInput.Normalize();
-        transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
+        playerInput = Vector2.ClampMagnitude(playerInput, 1.0f);
+        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
     }
 }
